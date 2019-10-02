0d3e1a6a903e166325720a2b73c6c756d40e74c9
ildar107 <ildar107wk@gmail.com>
2019-10-02 12:35:32 +0300

    feat: add decision

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor(min = null, max = null) {
+        this.min = min;
+        this.max = max;
+    }
 
     setRange(min, max) {
 
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
+        return Math.ceil((this.max - this.min)/2 + this.min);
 
     }
 
     lower() {
-
+        this.max =  Math.ceil((this.max - this.min)/2  + this.min);
     }
 
     greater() {
-
+        this.min =  Math.ceil((this.max - this.min)/2 + this.min);
     }
 }
 
